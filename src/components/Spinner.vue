baa1953a23e31ca3ed34131c767b24a5751d93c0
Shakeel <shakeeldvd23@gmail.com>
2024-08-19 13:18:43 +0200

    Components Updated

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,1 +1,12 @@
-<!-- spinner -->
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SpinnerComp'
+}
+</script>
+
