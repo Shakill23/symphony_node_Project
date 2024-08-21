f4dd0ead222b2eafa0fd23c86ab7bde811c24ad0
Chany4 <chandreleighdavids04052005@gmail.com>
2024-08-21 13:45:03 +0200

    edited

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {router} from './routes/productRoutes.js'
 import { routerUsers } from './routes/userRouter.js';
 import cors from 'cors';
+import path from 'path'
 
 let port = process.env.PORT ||7070
 const app = express()
@@ -14,11 +15,13 @@ app.use(cors())
 // }))
 
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('./public'))
 
 app.use('/products', router)
 app.use('/users', routerUsers)
-
+app.get('/', (req, res)=>{
+    res.status(200).sendFile(path.resolve('./public/index.html'))
+})
 app.listen(port,()=>{
     console.log('http://localhost:'+ port);
 })
