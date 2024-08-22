d4363ff0ee820ea55ea011c0e107df766a549b2e
Chany4 <chandreleighdavids04052005@gmail.com>
2024-08-22 10:13:56 +0200

    edit

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ import path from 'path'
 let port = process.env.PORT ||7070
 const app = express()
 
-// app.use(cors())
-
-// app.use(cors({
-//     origin: 'http://localhost:8081',
-//     credentials:true
-// }))
+app.use(cors({
+    origin: '*',
+    credentials:true
+}))
 
 app.use(express.json())
 app.use(express.static('./public'))
 
 app.use('/products', router)
 app.use('/users', routerUsers)
-app.get('/', (req, res)=>{
-    res.status(200).sendFile(path.resolve('./public/index.html'))
-})
+// app.get('/', (req, res)=>{
+//     res.status(200).sendFile(path.resolve('./public/index.html'))
+// })
 app.listen(port,()=>{
     console.log('http://localhost:'+ port);
 })
